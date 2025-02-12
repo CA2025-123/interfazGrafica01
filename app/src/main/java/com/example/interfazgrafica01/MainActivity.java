258a87787d81fc44cd455cf9f9bb381dd827ac6b
CA2025-123 <ca21024@ues.edu.sv>
2025-02-12 14:50:46 -0600

    Version 1

--- app/src/main/java/com/example/interfazgrafica01/MainActivity.java
+++ app/src/main/java/com/example/interfazgrafica01/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.interfazgrafica01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText texto1,direccion,resultado;
+    Button btnVer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        texto1 = findViewById(R.id.texto1);
+        direccion = findViewById(R.id.direccion);
+        resultado = findViewById(R.id.resultado);
+        btnVer = findViewById(R.id.btnVer);
+
+    }
+    public void guardar(View view){
+        if(texto1.getText().toString().isEmpty()){
+            Toast.makeText(getBaseContext(),"Vacio", Toast.LENGTH_SHORT).show();
+            texto1.setError("Esto esta Vacio");
+        }
+        resultado.setText(""+texto1.getText().toString());
     }
 }
